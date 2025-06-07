620f9bb2ad2adc6610bed13459e7782a2a78e834
Oliver-Form <oliverform@proton.me>
2025-06-07 21:44:47 +0100

    updated screen imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mission/pages/groceries_page.dart';
+import 'package:mission/screens/groceries_page.dart';
 
 void main() {
   runApp(const MyApp());
